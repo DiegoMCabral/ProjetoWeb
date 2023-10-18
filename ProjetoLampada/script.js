7fe0c008f41f26330f282c9b315668a1ae0ccdf0
DiegoMCabral <diego.merotti@gmail.com>
2023-10-17 22:24:46 -0300

    C:/Users/Alunos/Desktop/ProjetosWeb/

---
+++ ProjetoLampada/script.js
@@ -0,0 +1,12 @@
+
+const botaoLigar = document.getElementById('ligar');
+const botaoDesligar = document.getElementById('desligar');
+
+botaoLigar.addEventListener('click', function() {
+function = botaoLigar();
+lampada.src="img/luzLigada.gif"
+});
+
+botaoDesligar.addEventListener('click', function() {
+});
+
